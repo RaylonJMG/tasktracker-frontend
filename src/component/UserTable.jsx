878ab386ed01830154307d81bcf0871eb30e78f3
RaylonJMG <ryj2213@gmail.com>
2025-08-06 21:09:03 -0500

    Disabled unnecessary User components that are not necessary for Capstone Project requirements

--- src/component/UserTable.jsx
+++ src/component/UserTable.jsx
@@ -1,92 +1,96 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+/****
+ * UNNECESSARY FOR CAPSTONE PROJECT
+ */
 
-const baseUrl = import.meta.env.VITE_BASE_URL;
-const endpoint = "user";
+// import React, { useEffect, useState } from "react";
+// import { useNavigate, useParams } from "react-router-dom";
 
-export const UserTable = () => {
-	const navigate = useNavigate();
-	const [user, setUser] = useState([]);
+// const baseUrl = import.meta.env.VITE_BASE_URL;
+// const endpoint = "user";
 
-	const getAllUsers = async () => {
-		const url = baseUrl + endpoint;
-		const token = localStorage.getItem("token");
-		const result = await fetch(url, {
-			method: "GET",
-			headers: {
-				authorization: token,
-			},
-		});
-		const data = await result.json();
-		setUser(data);
-	};
-	const handleEdit = (id) => {
-		navigate(`/editUser/${id}`);
-	};
-	const handleDelete = async (id) => {
-		const url = `${baseUrl}${endpoint}/${id}`;
-		const token = localStorage.getItem("token");
-		const result = await fetch(url, {
-			method: "DELETE",
-			headers: {
-				authorization: token,
-			},
-		});
-		const data = await result.json();
-		window.location.reload();
-	};
-	const handleUsers = (id) => {
-		navigate(`/user/${id}`);
-	};
+// export const UserTable = () => {
+// 	const navigate = useNavigate();
+// 	const [user, setUser] = useState([]);
 
-	useEffect(() => {
-		getAllUsers();
-	}, []);
+// 	const getAllUsers = async () => {
+// 		const url = baseUrl + endpoint;
+// 		const token = localStorage.getItem("token");
+// 		const result = await fetch(url, {
+// 			method: "GET",
+// 			headers: {
+// 				authorization: token,
+// 			},
+// 		});
+// 		const data = await result.json();
+// 		setUser(data);
+// 	};
+// 	const handleEdit = (id) => {
+// 		navigate(`/editUser/${id}`);
+// 	};
+// 	const handleDelete = async (id) => {
+// 		const url = `${baseUrl}${endpoint}/${id}`;
+// 		const token = localStorage.getItem("token");
+// 		const result = await fetch(url, {
+// 			method: "DELETE",
+// 			headers: {
+// 				authorization: token,
+// 			},
+// 		});
+// 		const data = await result.json();
+// 		window.location.reload();
+// 	};
+// 	const handleUsers = (id) => {
+// 		navigate(`/user/${id}`);
+// 	};
 
-	return (
-		<>
-			<h1>User Registry</h1>
-			<div className="table-responsive">
-				<table className="table container text-align:center table-responsive">
-					<thead>
-						<tr>
-							<th>User Name</th>
-							<th>First Name</th>
-							<th>Last Name</th>
-							<th>Phone</th>
-							<th>Actions</th>
-						</tr>
-					</thead>
-					<tbody className="table-group-divider">
-						{user.map((item) => (
-							<tr key={item.user_id}>
-								<td>{item.username}</td>
-								<td>{item.first_name}</td>
-								<td>{item.last_name}</td>
-								<td>{item.phone}</td>
-								<td>
-									<button
-										type="button"
-										onClick={() => handleEdit(item.user_id)}
-										className="btn btn-primary">
-										<i className="bi bi-pencil-square"></i>
-									</button>
-									<button
-										type="button"
-										onClick={() => handleDelete(item.user_id)}
-										className="btn btn-danger">
-										<i className="bi bi-person-x"></i>
-									</button>
-									<button
-										type="button"
-										onClick={() => handleUsers(item.user_id)}
-										className="btn btn-info"></button>
-								</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</div>
-		</>
-	);
-};
+// 	useEffect(() => {
+// 		getAllUsers();
+// 	}, []);
+
+// 	return (
+// 		<>
+// 			<h1>User Registry</h1>
+// 			<div className="table-responsive">
+// 				<table className="table container text-align:center table-responsive">
+// 					<thead>
+// 						<tr>
+// 							<th>User Name</th>
+// 							<th>First Name</th>
+// 							<th>Last Name</th>
+// 							<th>Phone</th>
+// 							<th>Actions</th>
+// 						</tr>
+// 					</thead>
+// 					<tbody className="table-group-divider">
+// 						{user.map((item) => (
+// 							<tr key={item.user_id}>
+// 								<td>{item.username}</td>
+// 								<td>{item.first_name}</td>
+// 								<td>{item.last_name}</td>
+// 								<td>{item.phone}</td>
+// 								<td>
+// 									<button
+// 										type="button"
+// 										onClick={() => handleEdit(item.user_id)}
+// 										className="btn btn-primary">
+// 										<i className="bi bi-pencil-square"></i>
+// 									</button>
+// 									<button
+// 										type="button"
+// 										onClick={() => handleDelete(item.user_id)}
+// 										className="btn btn-danger">
+// 										<i className="bi bi-person-x"></i>
+// 									</button>
+// 									<button
+// 										type="button"
+// 										onClick={() => handleUsers(item.user_id)}
+// 										className="btn btn-info"></button>
+// 								</td>
+// 							</tr>
+// 						))}
+// 					</tbody>
+// 				</table>
+// 			</div>
+// 		</>
+// 	);
+// };
